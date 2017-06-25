098ae427803fd4c273b51dd4f3707805f49a6339
johnfosborneiii <johnfosborneiii@gmail.com>
2017-06-25 11:30:10 -0400

    Create getmyip.php

---
+++ getmyip.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>OpenShift in Action</title>
+ </head>
+ <body>
+ <h1>Hello Pod</h1>
+ <i>
+ <?php echo "Request served by: {$_SERVER['SERVER_ADDR']} at " , date("D M j G:i:s T Y"); ?>
+ </i>
+ </body>
+</html>
