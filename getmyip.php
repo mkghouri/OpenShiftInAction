fb871b32170cec1c5139cb3abf18282a733ba269
johnfosborneiii <johnfosborneiii@gmail.com>
2017-06-25 12:06:19 -0400

    Update getmyip.php

--- getmyip.php
+++ getmyip.php
@@ -1,11 +1,1 @@
-<html>
- <head>
-  <title>OpenShift in Action</title>
- </head>
- <body>
- <h1>Hello Pod</h1>
- <i>
- <?php echo "Request served by: {$_SERVER['SERVER_ADDR']} at " , date("D M j G:i:s T Y"); ?>
- </i>
- </body>
-</html>
+<?php echo "My IP: : {$_SERVER['SERVER_ADDR']} at " , date("D M j G:i:s T Y"); ?>
